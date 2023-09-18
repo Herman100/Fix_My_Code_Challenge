18c230b6b0794db9dc4eb90959efa92f3b579a11
Herman100 <hermankwamebour30@gmail.com>
2023-09-18 18:42:59 +0000

    pycodestyle fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,28 @@
-"""This module provide a Class to instance geometry objects"""
+#!/usr/bin/python3
+"""A class module to represent a square."""
 
 
 class Square():
-    """Provide methods fto calculate of the are and perimiter"""
+    """A class to represent a square."""
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Constructor definition"""
+        """Initialize square with given width."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area of the square"""
+        """Calculate and return the area of the square."""
         return self.width * self.height
 
-    def permiter_of_my_square(self):
-        """Permiter of my square"""
+    def perimeter_of_my_square(self):
+        """Calculate and return the perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Representing of my object"""
+        """Return a string representation of the square."""
         return "{}/{}".format(self.width, self.height)
 
 
@@ -28,4 +30,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.perimeter_of_my_square())
