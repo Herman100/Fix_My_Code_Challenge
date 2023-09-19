7777564010dc08e11dd7d99ed51e9e77bea1bcff
Herman100 <hermankwamebour30@gmail.com>
2023-09-19 23:01:09 +0000

    task added

--- 0x01-challenge/blog/app/models/post.rb
+++ 0x01-challenge/blog/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many  :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
+
+  def post_params
+    params.require(:post).permit(:title, :body, :online)
+
 end
